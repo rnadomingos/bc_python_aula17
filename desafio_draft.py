cc4318029f90a4934c53bd836c335ef9aa1609fe
Renan Domingos <rna.domingos@hotmail.com>
2025-05-11 23:23:39 -0300

    feat: add query in one table and join tables

--- desafio_draft.py
+++ desafio_draft.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Double, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Double, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
@@ -29,23 +29,35 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()    
 
-fonecedores = [
-  Fornecedor(nome="Fornecedor A", telefone="12345678", email="contato@a.com", endereco="Endereço A"),
-  Fornecedor(nome="Fornecedor B", telefone="87654321", email="contato@b.com", endereco="Endereço B"),
-  Fornecedor(nome="Fornecedor C", telefone="12348765", email="contato@c.com", endereco="Endereço C"),
-  Fornecedor(nome="Fornecedor D", telefone="56781234", email="contato@d.com", endereco="Endereço D"),
-  Fornecedor(nome="Fornecedor E", telefone="43217865", email="contato@e.com", endereco="Endereço E")
-]
-session.add_all(fonecedores)
-session.commit()
+# fonecedores = [
+#   Fornecedor(nome="Fornecedor A", telefone="12345678", email="contato@a.com", endereco="Endereço A"),
+#   Fornecedor(nome="Fornecedor B", telefone="87654321", email="contato@b.com", endereco="Endereço B"),
+#   Fornecedor(nome="Fornecedor C", telefone="12348765", email="contato@c.com", endereco="Endereço C"),
+#   Fornecedor(nome="Fornecedor D", telefone="56781234", email="contato@d.com", endereco="Endereço D"),
+#   Fornecedor(nome="Fornecedor E", telefone="43217865", email="contato@e.com", endereco="Endereço E")
+# ]
+# session.add_all(fonecedores)
+# session.commit()
 
-produtos = [
-  Produto(nome="Produto 1", descricao="Descrição do Produto 1", preco=100, fornecedor_id=1),
-  Produto(nome="Produto 2", descricao="Descrição do Produto 2", preco=200, fornecedor_id=2),
-  Produto(nome="Produto 3", descricao="Descrição do Produto 3", preco=300, fornecedor_id=3),
-  Produto(nome="Produto 4", descricao="Descrição do Produto 4", preco=400, fornecedor_id=4),
-  Produto(nome="Produto 5", descricao="Descrição do Produto 5", preco=500, fornecedor_id=5)
-]
+# produtos = [
+#   Produto(nome="Produto 1", descricao="Descrição do Produto 1", preco=100, fornecedor_id=1),
+#   Produto(nome="Produto 2", descricao="Descrição do Produto 2", preco=200, fornecedor_id=2),
+#   Produto(nome="Produto 3", descricao="Descrição do Produto 3", preco=300, fornecedor_id=3),
+#   Produto(nome="Produto 4", descricao="Descrição do Produto 4", preco=400, fornecedor_id=4),
+#   Produto(nome="Produto 5", descricao="Descrição do Produto 5", preco=500, fornecedor_id=5)
+# ]
 
-session.add_all(produtos)
-session.commit()
+# session.add_all(produtos)
+# session.commit()
+
+for produto in session.query(Produto).all():
+  print(f"Produto: {produto.nome}, Fornecedor: {produto.fornecedor.nome}")
+
+resultado = session.query(
+   Fornecedor.nome,
+   func.sum(Produto.preco).label('total_preco')
+).join(Produto, Fornecedor.id == Produto.fornecedor_id
+).group_by(Fornecedor.nome).all()
+
+for nome, total_preco in resultado:
+   print(f"Fornecedor: {nome}, Total Preço: {total_preco}")
