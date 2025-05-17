78900acf9500564ed6d71971c12b183844f49636
Renan Domingos <rna.domingos@hotmail.com>
2025-05-17 10:00:51 -0300

    feat: add database connection

---
+++ src/data/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+SQLALCHEMY_DATA_BASE_URL = 'sqlite:///desafio.db'
+engine = create_engine(SQLALCHEMY_DATA_BASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Relationship = relationship()
+Base = declarative_base()
